44323598ed754329fdf7964fdf2ff7490bf17e71
cristiii <cristian.vlad10@gmail.com>
2023-01-29 20:30:00 +0200

    updated application-name

---
+++ src/main/java/com/example/photoappusersservice/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.example.photoappusersservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/status/check")
+    public String status() {
+        return "working";
+    }
+}
