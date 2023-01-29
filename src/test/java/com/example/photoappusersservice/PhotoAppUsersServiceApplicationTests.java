6ea36ec8adcd46630977f9cb6e291e3977cdd21f
cristiii <cristian.vlad10@gmail.com>
2023-01-29 20:07:31 +0200

    init

---
+++ src/test/java/com/example/photoappusersservice/PhotoAppUsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoappusersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppUsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
