9a528a7fe6903c0c415948346cf3e4e674e5d6e3
cristiii <cristian.vlad10@gmail.com>
2023-01-29 20:15:47 +0200

    enable eureka client

--- src/main/java/com/example/photoappusersservice/PhotoAppUsersServiceApplication.java
+++ src/main/java/com/example/photoappusersservice/PhotoAppUsersServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.photoappusersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhotoAppUsersServiceApplication {
 
     public static void main(String[] args) {
