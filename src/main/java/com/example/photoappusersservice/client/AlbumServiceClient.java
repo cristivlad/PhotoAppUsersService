709120dcd75b5cd1550549dabd40c2311f7ed33b
cristiii <cristian.vlad10@gmail.com>
2023-02-07 16:34:27 +0200

    added circuit breaker from resilience4j and created a fallback method

--- src/main/java/com/example/photoappusersservice/client/AlbumServiceClient.java
+++ src/main/java/com/example/photoappusersservice/client/AlbumServiceClient.java
@@ -1,15 +1,22 @@
 package com.example.photoappusersservice.client;
 
 import com.example.photoappusersservice.model.AlbumResponseModel;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @FeignClient(name = "albums-service")
 public interface AlbumServiceClient {
 
     @GetMapping("/users/{id}/albums")
+    @CircuitBreaker(name = "albums-service", fallbackMethod = "getAlbumsFallback")
     List<AlbumResponseModel> getAlbums(@PathVariable String id);
+
+    default List<AlbumResponseModel> getAlbumsFallback(String id, Throwable exc) {
+        return new ArrayList<>();
+    }
 }
